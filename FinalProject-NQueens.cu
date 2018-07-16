01f061dab9514665b95b7f1e13af2829cb81bb75
Conner Wulf <33533907+connerwulf@users.noreply.github.com>
2018-07-16 19:49:18 -0400

    Working Code
    intial push

--- FinalProject-NQueens.cu
+++ FinalProject-NQueens.cu
@@ -18,10 +18,16 @@
 using namespace std;
 static int total = 0;
 unsigned long count = 0;
-struct timezone Idunno;	
+long *answer;
+struct timezone Idunno;
 struct timeval startTime, endTime;
+
+ #ifndef NUM
+ #define NUM 12
+ #endif
+
 //CPU helper function to test is a queen can be placed
-int isAllowed(int **board, int row, int col, int n) // make this the kernel?????
+int isAllowed(int **board, int row, int col, int n)
 {
   int x,y;
 
@@ -50,104 +56,7 @@ int isAllowed(int **board, int row, int col, int n) // make this the kernel?????
   }
  return 1;
 }
-
-// GPU helper problem
-
-/*
-__global__ void nqueen_kernel_3(*job_data, *results ... )
-{
-  __const__ tid; //The index of the thread within the block
-  __register__ rowIndex, solution, index;
-  __shared__ ROW[MAX_ROW][BLOCK_SIZE];
-  __const__ upper_bound = the upper bound of the job-pool for this block;
-  __shared__ seek;
-  if(tid == 0) { set seek to point to the next new job in the job-pool for this block;}
-  each thread fetches a task from job_data into its array ROW[MAX_ROW] [tid];
-  for(; rowIndex >= 0; rowIndex--) {
-  ĂĂ //the same code as in the low-divergence n-queens kernel is omitted
-  if (rowIndex == 0) { //current job is done.
-  index = atomicAdd(&seek,1); //get index of new job
-  if (index exceeds pool upper bound)
-  break;
-  else{
-  gets this job by index from the job-pool as the new job of this thread;
-  rowIndex++;
-  }
-  }
-  }
-  reduction of the solutions of the threads within this block;
-}
-*/
-
-/* use this one */
-// /*
-// __global__ void nqueen_kernel_0(int *job_data, int *results, int *work_space)
-// {
-//   __register__ rowIndex, solution;
-//   int tx = threadIdx.x;
-//   int x = tx * blockDim.x + threadIdx.x;
-
-//   each thread fetches a task from job_data into its array ROW[ ] in work_space;
-//   while(rowIndex >= 0) {
-//     if (no position to place new queen in ROW[rowIndex]) { rowIndex--; }
-//     else{
-//       finds a valid position P in ROW[rowIndex];
-//       places a queen at P in ROW[rowIndex] and mark the position as occupied;
-//     if (reaches last row) { solution++; }
-//     else{
-//       generates ROW[rowIndex+1] based on Row[rowIndex] and the position P;
-//       rowIndex++;
-//     }
-//   }
-// }
-// reduction of the solutions of the threads within each bl*/
-
-
-// __global__ void queenSolverGpu(int *d_board, int n, int *allowed, int *count) {
-//   int threadId = blockIdx.x * blockDim.x + threadIdx.x;
-//   int threadX = threadIdx.x;
-//   int qBitCol[n * blockDim.x];
-//   int qBitPosDiag[n * blockDim.x];
-//   int qBitNegDiag[n * blockDim.x];
-//   int stack[n*n+2];
-//   register int nStack;
-//   qBitCol[tx]=qBitPosDiag[tx]=qBitNegDiag[tx]=0;
-// }
-//N-queen solver for CPU algorithm
-// int SolverGPU(int **board, int col, int n)
-// {
-//   int *allowed;
-//   int temp = 0;
-//   int *d_board;
-//   dim3 threadsPerBlock(n, n);
-//   dim3 numBlocks(n / threadsPerBlock.x, n / threadsPerBlock.y);
-//   cudaMalloc((void **) &d_board, sizeof(int) * n);
-//   cudaMalloc((void **) &allowed, n);
-//   cudaMemcpy(allowed, &temp, sizeof(int), cudaMemcpyHostToDevice);
-//   cudaMemcpy(d_board, board[0], sizeof(int) * n * n, cudaMemcpyHostToDevice);
-
-//   if (col >= n * n)
-//   {
-//     total++;
-//     return 1;
-//   }
-
-//   int nextState = 0;
-//   for(int k = 0; k < n; k++)
-//   {
-//     isAllowedGpu<<<numBlocks, threadsPerBlock>>>(d_board, k, col, n * n, allowed);
-//     cudaMemcpy(&temp, allowed, sizeof(int), cudaMemcpyDeviceToHost);
-//     if(temp == 1)
-//     {
-//       board[k][col] = 1;
-//       nextState = Solver(board, col + 1, n) || nextState;
-//       board[k][col] = 0;
-//     }
-//   }
-
-//   return nextState;
-// }
-
+// CPU Solver for N-queens problem
 int Solver(int **board, int col, int n)
 {
   if (col >= n)
@@ -170,6 +79,115 @@ int Solver(int **board, int col, int n)
   return nextState;
 }
 
+// GPU parallel kernel for N-Queens
+__global__ void kernel(long *answer, int SegSize, int nBX, int nBY, int genNum)
+{
+  __shared__ long sol[NUM][NUM];
+  __shared__ char tup[NUM][NUM][NUM];
+
+  int wrongCount = 0;
+  sol[threadIdx.x][threadIdx.y] = 0;
+  tup[threadIdx.x][threadIdx.y][0] = blockIdx.y % SegSize;
+  int totalGenerated = powf(NUM, genNum);
+  int blockYSeg = blockIdx.y / SegSize;
+  int workLoad = totalGenerated / nBY;
+  int runOff = totalGenerated - workLoad *nBY;
+
+
+
+
+  int temp = blockIdx.x;
+  for(int x = 1; x <=nBX; x++)
+  {
+    tup[threadIdx.x][threadIdx.y][x] = temp % NUM;
+    temp = temp / NUM;
+
+  }
+
+  int tupCount = nBX;
+  tup[threadIdx.x][threadIdx.y][++tupCount] = threadIdx.x;
+  tup[threadIdx.x][threadIdx.y][++tupCount] = threadIdx.y;
+
+  for(int k = tupCount; k > 0; k--)
+  {
+    for(int m = k - 1, counter = 1; m >= 0; counter++, m--)
+    {
+      //Checks diagonal left, down
+      wrongCount += (tup[threadIdx.x][threadIdx.y][k] + counter) == tup[threadIdx.x][threadIdx.y][m];
+      //Checks row its in
+      wrongCount += tup[threadIdx.x][threadIdx.y][k] == tup[threadIdx.x][threadIdx.y][m];
+      // Checks diagonal left, up
+      wrongCount  += (tup[threadIdx.x][threadIdx.y][k] - counter) == tup[threadIdx.x][threadIdx.y][m];
+
+    }
+  }
+
+
+
+
+
+
+  if (wrongCount == 0)
+  {
+    int begin = blockYSeg * workLoad;
+    for(int c = begin; c < begin + workLoad + (blockYSeg == nBY - 1) * runOff; c++)
+    {
+      //last values is made in tuple, convert and store to tup array
+      int temp = c;
+      for(int q = 0, z =tupCount + 1; q < genNum; z++, q++)
+      {
+        tup[threadIdx.x][threadIdx.y][q] = temp % NUM;
+        temp = temp / NUM;
+      }
+
+      //checks that the genNum tuple values are indeed unique (saves work overall)
+      for(int a = 0; a < genNum && wrongCount == 0; a++){
+				for(int b = 0; b < genNum && wrongCount == 0; b++){
+					wrongCount += tup[threadIdx.x][threadIdx.y][tupCount + 1 + a] == tup[threadIdx.x][threadIdx.y][tupCount + 1 + b] && a != b;
+				}
+			}
+
+      for(int k = NUM -1; k > wrongCount; k--)
+      {
+        for(int m = k - 1, counter = 1; m >= 0; counter++, m--)
+        {
+          //Checks diagonal left, down
+          wrongCount += (tup[threadIdx.x][threadIdx.y][k] + counter) == tup[threadIdx.x][threadIdx.y][m];
+          //Checks row its in
+          wrongCount += tup[threadIdx.x][threadIdx.y][k] == tup[threadIdx.x][threadIdx.y][m];
+          // Checks diagonal left, up
+          wrongCount  += (tup[threadIdx.x][threadIdx.y][k] - counter) == tup[threadIdx.x][threadIdx.y][m];
+
+        }
+      }
+
+      sol[threadIdx.x][threadIdx.y] += !(wrongCount);
+      wrongCount = 0;
+
+    }
+  }
+
+  __syncthreads();
+
+    // sum all threads in block to get total
+  	if(threadIdx.x == 0 && threadIdx.y == 0)
+    {
+
+  		long total = 0;
+
+  		for(int i =0; i < NUM; i++){
+  			for(int j = 0; j < NUM; j++){
+  				total += sol[i][j];
+  			}
+  		}
+  		answer[gridDim.x * blockIdx.y + blockIdx.x] = total;
+  	}
+
+
+  	__syncthreads();
+}
+
+
 double report_running_time() {
 	long sec_diff, usec_diff;
 	gettimeofday(&endTime, &Idunno);
@@ -179,75 +197,104 @@ double report_running_time() {
 		sec_diff --;
 		usec_diff += 1000000;
 	}
-	printf("Running time for CPU version: %ld.%06ld\n", sec_diff, usec_diff);
+	printf("CPU Time: %ld.%06ld\n", sec_diff, usec_diff);
 	return (double)(sec_diff*1.0 + usec_diff/1000000.0);
 }
 
+
 int main(int argc, char **argv) {
-  //  CPU VERSION
-  
-  const int n = atoi(argv[1]);
-  int **board;
-  int **newHostBoard;
-  newHostBoard = (int **) malloc(n * sizeof(int *));
-  board = (int **) malloc(n * sizeof(int *));
-  for (int i = 0; i < n; i++) {
-    board[i] = (int *) malloc(n * sizeof(int));
-    newHostBoard[i] = (int *) malloc(n * sizeof(int));
+
+  if(argc < 3) {
+
+    printf("\nError, too few arguments. Usage: ./CHANGE THIS\n");
+    return -1;
   }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+
+  const int NUM_TUPLEX = atoi(argv[1]);
+  const int NUM_TUPLEY = atoi(argv[2]);
+  const int generatedNum = NUM - 3 - NUM_TUPLEX;
+  cudaEvent_t start, stop;
+  float elapsedTime;
+
+  if(generatedNum < 0){
+    printf("\nThe numbers generated iteratively cannot be less than 0.\n");
+    exit(1);
+  }
+
+  //ensure N is in the correct range
+  if(NUM < 4  || NUM > 22){
+    printf("\nN(%d) must be between 4 and 22 inclusive\n", NUM);
+    exit(1);
+  }
+
+  //ensure that at least one of the tuple values is generated by the block's X coordinate value
+  if(NUM_TUPLEX < 1){
+    printf("\nThe number of tuples generated by each block's X coordinate value must be >= 1\n");
+    exit(1);
+  }
+
+  	//ensure that the number of Y segments that the numGen work is divided into
+  	//is at least one per work segment
+  	if(NUM_TUPLEY > pow(NUM, generatedNum)){
+  		printf("\n number of groups of columns must be less than or equal to N^(N - 3 - (1st ARG))\n");
+  		exit(1);
+  	}
+
+  //CPU setup
+  int **board;
+  board = (int **) malloc(NUM * sizeof(int *));
+
+  for (int i = 0; i < NUM; i++) {
+    board[i] = (int *) malloc(NUM * sizeof(int));
+
+  }
+  for (int i = 0; i < NUM; i++) {
+    for (int j = 0; j < NUM; j++) {
       board[i][j] = 0;
+
     }
   }
-	// vector<vector<int> > board;
-  // board.resize(n, std::vector<int>(n, 0));
-  // int *allowed;
-  // int temp = 0;
-  // int *d_board;
-  // int *count;
-  // size_t pitch;
-  // dim3 threadsPerBlock(n, 1, 1);
-  // dim3 numBlocks(n / threadsPerBlock.x, 1, 1);
-  // cudaMalloc((void **) &allowed, n);
-  // cudaMalloc((void **) &count, n);
-  // cudaMallocPitch(&d_board, &pitch, n * sizeof(int), n);
-  // cudaMemcpy2D(d_board, pitch, board, n * sizeof(int), n * sizeof(int), n, cudaMemcpyHostToDevice);
-  // cudaMemcpy(allowed, &temp, sizeof(int), cudaMemcpyHostToDevice);
-  // cudaMemcpy(count, &total, sizeof(int), cudaMemcpyHostToDevice);
-  // // for(int i = 0; i < n; i++) {
-  // //   for(int k = 0; k < n; k++) {
-  //     isAllowedGpu<<<numBlocks, threadsPerBlock, n>>>(d_board, n, 0, n, allowed, count);
-  //     cudaMemcpy(&temp, allowed, sizeof(int), cudaMemcpyDeviceToHost);
-  //     if(temp == 1) {
-  //       newHostBoard[i][k] = 1;
 
-  //     }
-  //   }
-  // }
-  // cudaMemcpy(&total, count, sizeof(int), cudaMemcpyDeviceToHost);
-  // cudaMemcpy2D(newHostBoard, pitch, d_board, n * sizeof(int), n * sizeof(int), n, cudaMemcpyDeviceToHost);
-	
-	
-  // if(temp == 0) {
-  //   printf("No Solution\n");
-  //   report_running_time();
-  //   return 0;
-  // }
+  int WIDTH, HEIGHT, NUM_BLOCKS, YSegmentSize;
+  WIDTH = pow(NUM, NUM_TUPLEX);
+  YSegmentSize = (NUM / 2) + (NUM % 2);
+  HEIGHT = YSegmentSize + NUM_TUPLEY;
+  NUM_BLOCKS = WIDTH * HEIGHT;
+
+
+  long *d_answer;
+  answer = new long[NUM_BLOCKS];
+
+  cudaMalloc((void **) &d_answer, sizeof(long) * NUM_BLOCKS);
+
+  dim3 block(NUM, NUM); //threads w x h
+  dim3 grid(WIDTH, HEIGHT); //blocks w x h
+
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start, 0);
+
+  kernel<<<grid, block>>>(d_answer, YSegmentSize, NUM_TUPLEX, NUM_TUPLEY, generatedNum);
+  cudaThreadSynchronize();
+
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsedTime, start, stop);
+
+  cudaMemcpy(answer,d_answer, sizeof(long) * NUM_BLOCKS, cudaMemcpyDeviceToHost);
+
+
+
+
 
 	srand(1);
   gettimeofday(&startTime, &Idunno);
-  Solver(board, 0, n);
-  
-  // if(Solver(board,0,n) == 0)
-  // {
-  //   printf("No Solution\n");
-  // 	report_running_time();
-  //   return 0;
-  // }
-  printf("\nTotal Solutions(CPU): %d boards\n\n",total);
-  report_running_time();
+  Solver(board, 0, NUM);
 
+
+  printf("\nTotal Solutions: %d boards\n\n",total);
+  report_running_time();
+  printf("GPU Time: %f secs\n", (elapsedTime / 1000.00));
   return 0;
 
 }
